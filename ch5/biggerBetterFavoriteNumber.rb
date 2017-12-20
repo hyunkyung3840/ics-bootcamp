86f677bbd3fd672c769c75e9a3e673734a742d52
hyunkyung3840 <34287186+hyunkyung3840@users.noreply.github.com>
2017-12-20 00:36:36 -0800

    Delete biggerBetterFavoriteNumber.rb

--- ch5/biggerBetterFavoriteNumber.rb
+++
@@ -1,4 +0,0 @@
-puts 'What is your favorite number?'
-favnum = gets.chomp
-newfavnum = favnum.to_i + 1
-puts 'Hmmmmmm..... I know a lot about numbers, and I think your new favorite number should be ' + newfavnum.to_s + '.'

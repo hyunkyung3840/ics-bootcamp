8503702cbeb99d7ed495d17da4e97a0743010a98
Hyunkyung <hyun_3840@berkeley.edu>
2017-12-20 00:32:49 -0800

    Bigger and better Favorite NUmber

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+favnum = gets.chomp
+newfavnum = favnum.to_i + 1
+puts 'Hmmmmmm..... I know a lot about numbers, and I think your new favorite number should be ' + newfavnum.to_s + '.'

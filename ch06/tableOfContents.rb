f563df83f7d2e7d03c28364248f623f1bb544d4e
Hyunkyung <hyun_3840@berkeley.edu>
2017-12-26 20:28:22 -0800

    Table of Contents

---
+++ ch06/tableOfContents.rb
@@ -0,0 +1,6 @@
+width = 70
+puts 'Table of Contents'.center(width)
+puts ''
+puts 'Chapter 1:'.ljust(width/4) +'Getting Started'.ljust(width/3) + 'page'.rjust(width/4) + '1'.rjust(width/6)
+puts 'Chapter 2:'.ljust(width/4) +'Numbers'.ljust(width/3) + 'page'.rjust(width/4) + '9'.rjust(width/6)
+puts 'Chapter 3:'.ljust(width/4) +'Letters'.ljust(width/3) + 'page'.rjust(width/4) + '13'.rjust(width/6)

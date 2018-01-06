ce074087a49dbc54e4e5ed72a2b5849d11804127
Hyunkyung <hyun_3840@berkeley.edu>
2018-01-05 20:10:17 -0800

    Table of Contents

--- ch08/tableOfContents.rb
+++ ch08/tableOfContents.rb
@@ -1,9 +1,11 @@
 width = 70
-ch1 = ['Chapter 1', 'Getting started', 'page', '1']
-ch2 = ['Chapter 2', 'Numbers', 'page', '9']
-ch3 = ['Chapter 3', 'Letters', 'page', '13']
+array = [['Getting started', '1'], ['Numbers', '9'], ['Letters', '13']]
 puts 'Table of Contents'.center(width)
 puts ''
-puts ch1[0].ljust(width/4) + ch1[1].ljust(width/3) + ch1[2].rjust(width/4) + ch1[3].rjust(width/6)
-puts ch2[0].ljust(width/4) +ch2[1].ljust(width/3) + ch2[2].rjust(width/4) + ch2[3].rjust(width/6)
-puts ch3[0].ljust(width/4) +ch3[1].ljust(width/3) + ch3[2].rjust(width/4) + ch3[3].rjust(width/6)
+chapnum = 1
+array.each do |chap|
+  chname = chap[0]
+  chpage = chap[1]
+  puts ('Chapter ' + chapnum.to_s + ':').ljust(width/4) + chname.ljust(width/3) + 'page'.rjust(width/4) + chpage.rjust(width/6)
+  chapnum = chapnum + 1
+end

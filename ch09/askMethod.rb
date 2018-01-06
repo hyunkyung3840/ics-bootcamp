0d7222c0aa0f593d6f08ae02cddac60e56b6814b
Hyunkyung <hyun_3840@berkeley.edu>
2018-01-05 20:04:16 -0800

    Ask Method

---
+++ ch09/askMethod.rb
@@ -0,0 +1,14 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if reply == 'yes'
+      return 'true'
+    end
+    if reply == 'no'
+      return 'false'
+    end
+    puts 'Please answer "yes" or "no".'
+  end
+end
+puts (ask 'Do you wet the bed?')

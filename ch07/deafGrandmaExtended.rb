6cf91eb14c3ed318e675155b523439f0a6456b2a
Hyunkyung <hyun_3840@berkeley.edu>
2018-01-05 21:02:32 -0800

    Deaf Grandma Extended

--- ch07/deafGrandmaExtended.rb
+++ ch07/deafGrandmaExtended.rb
@@ -1,28 +1,20 @@
-while true
+bye = 0
+puts 'HELLO SWEETIE! HOW ARE YOU TODAY?'
+while bye < 3
   my = gets.chomp
   if my == 'BYE'.chomp
-    my1 = gets.chomp
-    if my1 == 'BYE'.chomp
-      my2 = gets.chomp
-      if my2 == 'BYE'.chomp
-        puts 'OKAY GOODBYE HONEY'
-        break
-      elsif my2 == my2.upcase
-        number = rand(19) + 1931
-        puts 'NO, NOT SINCE ' + number.to_s
-      else
-        puts 'HUH?! SPEAK UP, SONNY!'
-      end
-    elsif my1 == my1.upcase
-      number = rand(19) + 1931
-      puts 'NO, NOT SINCE ' + number.to_s
+    bye = bye + 1
+    if bye == 3
+      puts 'OKAY GOODBYE HONEY'
     else
-      puts 'HUH?! SPEAK UP, SONNY!'
+      puts 'Huh?'
     end
   elsif my == my.upcase
     number = rand(19) + 1931
     puts 'NO, NOT SINCE ' + number.to_s
+    bye = 0
   else
     puts 'HUH?! SPEAK UP, SONNY!'
+    bye = 0
   end
 end
